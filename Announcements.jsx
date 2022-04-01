7584c841a59234adfa70ee2a55a1252b78d5b0e8
tbayn <79238799+tbayn@users.noreply.github.com>
2022-04-01 10:13:43 -0400

    Add files via upload

---
+++ Announcements.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Announcements = () => {
+  return (
+    <>
+      <h5 id = "Announcement">
+       Weekly Announcements 
+      </h5>
+    </>
+  )
+}
+
+export default Announcements
---
+++ cart.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const cart = () => {
+  return (
+    <main>
+        <form>
+        <h2>??? items in your cart</h2>
+        <img src = "" alt = ""></img>
+        <span>Price</span>
+        <button>Remove</button>
+        <label>
+            <textarea></textarea>
+        </label>
+        <input type = "submit"></input>
+        </form>
+    </main>
+  )
+}
+
+export default cart
